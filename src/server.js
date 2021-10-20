bd0a90516d17a8b654cf626be0d00e53bbc23070
grigory-lebedev <lebedevgrisha21@gmail.com>
2021-10-20 09:42:52 +0300

    fixed port

--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ const initExpress = require('./loaders/express');
 const initRoutes = require('./loaders/routes');
 const initMiddlewares = require('./loaders/middlewares');
 const cors = require('./middlewares/cors');
+const port = process.env.PORT || 3000;
 
 exports.start = () => {
   initExpress(app);
   app.use(cors);
   initRoutes(app);
   initMiddlewares(app);
-  app.listen(3000, () => console.log('Server is running on the port 3000'));
+  app.listen(port, () => console.log(`Server is running on the port ${port}`));
 }
